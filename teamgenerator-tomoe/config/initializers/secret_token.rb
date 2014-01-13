f1b7080f59b06cec9656e64e2686398933755250
molybdenum0330 <molybdenum_0330@yahoo.co.jp>
2014-01-14 02:27:56 +0900

    initial commit.

---
+++ teamgenerator-tomoe/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamgeneratorTomoe::Application.config.secret_key_base = '1077961e790836ef727b70a1b0ef6c4abc6ab68cd434eb8f468b31a5130dd6d1b877017182e4b6efdd69f1ec2ff09d7c93363d7da892c59982f4ba0594920418'
