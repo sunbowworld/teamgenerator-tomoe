7b4b35ee15b6aed5a5ef8133de1a4ae34f3349fa
molybdenum0330 <molybdenum0330@gmail.com>
2014-01-22 00:35:18 +0900

    twitter-bootstrap3の導入及びapplication.html.hamlの実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def steam_card(steam_id)
     render :inline => <<-HAML, :type => :haml, :locals => {:steam_id => steam_id}
-.steam_id
+.steam_id{id: steam_id.steam_id64}
   .avator
     = image_tag steam_id.icon_url
   .name
