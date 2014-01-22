1f25b81bb17fe35100baf161be109c57ee6385ca
molybdenum0330 <molybdenum0330@gmail.com>
2014-01-23 01:36:44 +0900

    steam_cardのエリア移動を実装

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  var tg_users = new Array()
+  $('.steam_card').click(function(){
+    switch($(this).parent().attr('class')){
+      case 'user_list':
+        $(this).appendTo('.tg_user')
+        break
+      case 'tg_user':
+        $(this).appendTo('.user_list')
+        break
+      default:
+        $(this).appendTo('.user_list')
+        break
+    }
+  })
+  $('.generator').click(function(){
+  })
+})
