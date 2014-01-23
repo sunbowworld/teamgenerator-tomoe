0cae0ea6757472e5e63f0a8eed0ff9740f07a712
molybdenum0330 <molybdenum0330@gmail.com>
2014-01-24 01:22:14 +0900

    チーム設定用UI追加

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,8 +2,15 @@
 // All this logic will automatically be available in application.js.
 $(function(){
   $('.steam_card').click(function(){
-    switchClassParent($(this),'.user_list','.tg_user')
+    switchClassParent($(this),'.user_list','.tg_area')
   })
   $('.generator').click(function(){
+    var users = $('.tg_user').children('.steam_card')
+  })
+  $('.teams_value').click(function(){
+    $('.number_of_teams > .value').text($(this).text())
+  })
+  $('.users_value').click(function(){
+    $('.number_of_users > .value').text($(this).text())
   })
 })
