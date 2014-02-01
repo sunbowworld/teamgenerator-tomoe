02f8b1f91e5fd1c9aece8a8538a92aa1f74b7d94
molybdenum0330 <molybdenum0330@gmail.com>
2014-02-02 05:22:49 +0900

    ユーザリストをサイドバーに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
     = steam_id.nickname
     HAML
   end
+
+  def steam_card_sidebar(steam_id)
+    render :inline => <<-HAML, :type => :haml, :locals => {:steam_id => steam_id}
+.steam_card.well{id: steam_id.steam_id64}
+  .avator
+    = image_tag steam_id.icon_url
+  .name
+    = steam_id.nickname
+    HAML
+  end
 end
