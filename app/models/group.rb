adb2c2616a1a0c500edf305dea94b79a584dfcf3
tomoe <tomoe@localhost.localdomain>
2014-02-03 03:31:43 +0900

    groupとteamのvalidationを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :teams
   has_and_belongs_to_many :steam_users
+
+  validates :name, presence: true
+  validates :name, length: { maximum: DB_STRING_MAX }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,7 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :versus_informations
   has_many :teams_versus_informations
   belongs_to :group
+
+  validates :name, presence: true
+  validates :name, length: { maximum: DB_STRING_MAX }
 end
