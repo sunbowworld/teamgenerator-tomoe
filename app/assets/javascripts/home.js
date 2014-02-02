09d0f2d967525c310cb2d48a8e33c6616336d9e7
molybdenum0330 <molybdenum0330@gmail.com>
2014-02-03 01:04:31 +0900

    チーム送信機能を実装

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,15 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(function(){
+
+  var teams
+
   $('.steam_card').click(function(){
     switchClassParent($(this),'.user_list','.tg_user')
   })
+
+  $('.teams_value').click(function(){
+    $('.number_of_teams > .value').text($(this).text())
+  })
+
   $('.generator').click(function(){
     var teamValue = $('.number_of_teams > .value').text()
-    //var userValue = $('.number_of_users > .value').text()
-    var teams = []
     var tmp
     var users = []
+    teams = []
 
     $('.tg_area .steam_card').appendTo($('.tg_user'))
     $('.tg_result').empty()
@@ -28,7 +35,7 @@ $(function(){
     }
 
     for (var i=teams.length; i--;){
-      $('.tg_result').prepend($('<div />', {id: "team"+(i+1),class: "col-lg-6"}))
+      $('.tg_result').prepend($('<div />', {id: "team"+(i+1),class: "col-lg-"+12/teams.length}))
       $('#team'+(i+1)).append($('<div />', {class: "panel panel-primary"}))
       $('#team'+(i+1)+' > .panel').append($('<div />',{class: "panel-heading",text: "team"+(i+1)}))
       $('#team'+(i+1)+' > .panel').append($('<div />',{class: "panel-body"}))
@@ -37,7 +44,10 @@ $(function(){
       }
     }
   })
-  $('.teams_value').click(function(){
-    $('.number_of_teams > .value').text($(this).text())
+
+  $('#submit').click(function(){
+    $("#form").append($('<input/>', {type: 'hidden', name: 'json', value: JSON.stringify({teams: teams})}))
+    $("#form").submit()
   })
+
 })
