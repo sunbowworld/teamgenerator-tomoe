f3680a1d8a8781b266d525e404eb9e41375b2a5b
tomoe <tomoe@localhost.localdomain>
2014-02-03 02:38:34 +0900

    steam_user/steam_id64のvalidationを追加

--- app/models/steam_user.rb
+++ app/models/steam_user.rb
@@ -1,7 +1,19 @@
 class SteamUser < ActiveRecord::Base
   has_and_belongs_to_many :teams
   has_and_belongs_to_many :groups
+
+  validate :steam_id64_check
+
   def get_SteamId
     SteamId.new steam_id64.to_i
   end
+
+  def steam_id64_check
+    begin
+      SteamId.new steam_id64
+    rescue
+      errors.add(:api, $!.to_s)
+    end
+  end
+  
 end
