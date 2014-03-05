016fc13c154ed422dcd629ff551a64a88ace57e7
molybdenum0330 <molybdenum0330@gmail.com>
2014-03-05 23:44:52 +0900

    コントローラ毎に読み込むjavascriptを変えるよう変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.min
-//= require_tree .
 
 function switchClassParent(obj,defaultParent,targetParent){
     var isInDefaultParent = '.' + obj.parent().attr('class')===defaultParent
